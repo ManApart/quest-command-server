0d8c80c4681bf1a56e704c603684e0cf21d89987
ManApart <r@austinkucera.com>
2021-10-09 20:40:50 -0400

    parsing params and body

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Quest Command Server
 
-Swagger: `http://localhost:8080/swagger-ui/`
+Example calls
+```
+curl localhost:8080/history/0?start=1
+curl -X POST localhost:8080/command/0?start=2 -d 'test and stuff' 
+```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import io.ktor.application.*
 import io.ktor.server.netty.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
@@ -8,18 +9,21 @@ import io.ktor.server.engine.*
 fun main() {
     embeddedServer(Netty, 8080) {
         routing {
-            get("/healthcheck") {
+            get("/health") {
                 call.respondText("Healthy", ContentType.Text.Html)
             }
 
             get("/history/{id}") {
                 val id = call.parameters["id"]
-                call.respondText("Healthy $id", ContentType.Text.Html)
+                val start = call.request.queryParameters["start"] ?: 0
+                call.respondText("Healthy $id $start", ContentType.Text.Html)
             }
 
             post("/command/{id}") {
                 val id = call.parameters["id"]
-                call.respondText("post $id", ContentType.Text.Html)
+                val start = call.request.queryParameters["start"] ?: 0
+                val body = call.receive<String>()
+                call.respondText("post $id $start $body", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
