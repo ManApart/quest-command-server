e5a399bd85b403a6a661074f70f63d9f118f8304
ManApart <r@austinkucera.com>
2021-10-09 20:26:42 -0400

    basic call structure

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,21 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 
-fun main(args: Array<String>) {
+fun main() {
     embeddedServer(Netty, 8080) {
         routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
+            get("/healthcheck") {
+                call.respondText("Healthy", ContentType.Text.Html)
+            }
+
+            get("/history/{id}") {
+                val id = call.parameters["id"]
+                call.respondText("Healthy $id", ContentType.Text.Html)
+            }
+
+            post("/command/{id}") {
+                val id = call.parameters["id"]
+                call.respondText("post $id", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
