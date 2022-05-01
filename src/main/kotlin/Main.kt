82909f79dcd521aa07f422f73e2190fbcb188c8e
ManApart <r@austinkucera.com>
2022-05-01 08:23:46 -0400

    proper latest response

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -70,8 +70,8 @@ private suspend fun ApplicationCall.respondWithHistory(name: String, player: Pla
     } else {
         Pair(0, listOf("No Player found for id $name."))
     }
+    println("History for ${player?.name ?: name}. $start - $end:")
     if (history.isNotEmpty()) {
-        println("History for ${player?.name ?: name}:")
         history.forEach { println("\t$it") }
     }
     this.respond(ServerResponse(end, history))
